65bd02257cb0a0288671f7c43901eb538ee9bb72
Zenzlia <natalie.hong@uqconnect.edu.au>
2021-03-17 11:54:55 +1000

    Latest version takes UDP and cries

--- Part C/RUSHBSvr.py
+++ Part C/RUSHBSvr.py
@@ -3,38 +3,26 @@ import sys
  
 LOCALHOST = "127.0.0.1"
  
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-try:
-	sock.bind((LOCALHOST, 0))
-	sock.listen(1)                                           
+sock.bind((LOCALHOST, 0))                                     
+print(sock.getsockname()[1])
 
-	while True:
-		print(sock.getsockname()[1])
+while True:
+	data, address = sock.recvfrom(1500)
+	print(data)
 
-		connection, client_address = sock.accept()      
-		print("Got a connection from %s" % str(client_address))
-	    
-		try:
-			print('connection from', client_address)
-			# Receive the data in small chunks and retransmit it
-			while True:
-				data = connection.recv(16)
-				print(data)
-				if data:
-					connection.sendall(data)
-				else:
-					print('no more data from {}'.format(client_address))
-					break
-		finally:
-			# Clean up the connection
-			msg = 'Thank you for connecting'+ "\r\n"
-			connection.send(msg.encode('ascii'))
-			connection.close()
 
-except socket.error as msg:
-	print('Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1])
-	sys.exit()
+
+
+
+
+	if data.decode().rstrip() == 'go away':
+		# Close the connection code
+		msg = str.encode("Thank you for connecting\n")
+		sock.sendto(msg, address)
+		sock.close()
+		break
 	
 print('Socket bind complete')
 
